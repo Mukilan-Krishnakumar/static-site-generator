9bb16fabceb5f268ec7e0e504efd61db15284ef0
Mukilan Krishnakumar <mukilan.git@gmail.com>
2024-03-12 09:55:03 +0530

    Minor Fix

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,9 @@ from htmlnode import HTMLNode
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag=None, props=None):
+    def __init__(self, tag=None, value=None, props=None):
+        if value == None:
+            raise ValueError
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
