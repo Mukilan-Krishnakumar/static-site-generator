8f2043ee3f7033746fedd17623e3f61d5c2b0032
Mukilan Krishnakumar <mukilan.git@gmail.com>
2024-03-13 07:53:15 +0530

    Made the text type more extensible with Enum

--- src/convert_textnode_to_htmlnode.py
+++ src/convert_textnode_to_htmlnode.py
@@ -1,19 +1,19 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 from leafnode import LeafNode
 
 
 def text_node_to_html_node(text_node):
-    if text_node.text_type == "text_type_text":
+    if text_node.text_type == TextType.text_type_text:
         return LeafNode(value=text_node.text)
-    elif text_node.text_type == "text_type_bold":
+    elif text_node.text_type == TextType.text_type_bold:
         return LeafNode("b", text_node.text)
-    elif text_node.text_type == "text_type_italic":
+    elif text_node.text_type == TextType.text_type_italic:
         return LeafNode("i", text_node.text)
-    elif text_node.text_type == "text_type_code":
+    elif text_node.text_type == TextType.text_type_code:
         return LeafNode("code", text_node.text)
-    elif text_node.text_type == "text_type_link":
+    elif text_node.text_type == TextType.text_type_link:
         return LeafNode("a", text_node.text, {"href": text_node.url})
-    elif text_node.text_type == "text_type_image":
+    elif text_node.text_type == TextType.text_type_image:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     else:
         raise Exception("Not a valid text type")
