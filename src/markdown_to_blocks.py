b631e08b31e4b86211398e2e24ae2bb34d52d397
Mukilan Krishnakumar <mukilan.git@gmail.com>
2024-03-19 08:04:07 +0530

    Return Block Types and Tests

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    block_type_paragraph = "paragraph"
+    block_type_heading = "heading"
+    block_type_code = "code"
+    block_type_quote = "quote"
+    block_type_unordered_list = "unordered_list"
+    block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     split_blocks = markdown.split("\n\n")
     mkdwn_blocks = []
@@ -10,3 +23,26 @@ def markdown_to_blocks(markdown):
         mkdwn_blocks.append(curr_block)
     # print(mkdwn_blocks)
     return mkdwn_blocks
+
+
+def block_to_block_type(block):
+    num_list = [str(n) for n in range(1, len(block) + 1)]
+    created_num_list = []
+    for num, text in enumerate(block):
+        if re.match(r"(?<!#)#{1,6} \w*", text) != None:
+            return BlockType.block_type_heading
+        num_search = re.search(f"^(\d). ", text)
+        if num_search != None:
+            created_num_list.append(num_search.group(1))
+    if created_num_list == num_list:
+        return BlockType.block_type_ordered_list
+    if block[0][:3] == "```" and block[-1][-3:] == "```":
+        return BlockType.block_type_code
+    initial_characters = [word[0] for word in block]
+    if all(flag == ">" for flag in initial_characters):
+        return BlockType.block_type_quote
+    elif all(flag == "*" for flag in initial_characters):
+        return BlockType.block_type_unordered_list
+    elif all(flag == "-" for flag in initial_characters):
+        return BlockType.block_type_unordered_list
+    return BlockType.block_type_paragraph
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_to_blocks import markdown_to_blocks
+from markdown_to_blocks import markdown_to_blocks, BlockType, block_to_block_type
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -40,3 +40,51 @@ class TestMarkdownToBlocks(unittest.TestCase):
             ["* This is a list item", "* This is another list item"],
         ]
         self.assertEqual(blocks, check_blocks)
+
+    def test_block_to_blocktype(self):
+        blocks = [
+            ["# This is a heading"],
+            [
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
+            ],
+            ["> A block here", "> Is a block there"],
+            ["```Me testing code```"],
+            ["1. Hello Internet", "2. Welcome to Film Theory", "3. This is Matpat"],
+            ["- Hello Again", "- Hihi"],
+        ]
+        check_types = [
+            BlockType.block_type_heading,
+            BlockType.block_type_paragraph,
+            BlockType.block_type_quote,
+            BlockType.block_type_code,
+            BlockType.block_type_ordered_list,
+            BlockType.block_type_unordered_list,
+        ]
+        block_types = []
+        for text_b in blocks:
+            block_types.append(block_to_block_type(text_b))
+        self.assertEqual(block_types, check_types)
+
+    def test_block_to_blocktype_combination(self):
+        blocks = [
+            ["####### This is not a heading"],
+            [
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
+            ],
+            ["> A block here", "* Becomes a paragraph there"],
+            ["`Me testing code without formatting```"],
+            ["1. Hello Internet", "-. Welcome to Film Theory", "3. This is Matpat"],
+            ["* Hello Again", "- Hihi"],
+        ]
+        check_types = [
+            BlockType.block_type_paragraph,
+            BlockType.block_type_paragraph,
+            BlockType.block_type_paragraph,
+            BlockType.block_type_paragraph,
+            BlockType.block_type_paragraph,
+            BlockType.block_type_paragraph,
+        ]
+        block_types = []
+        for text_b in blocks:
+            block_types.append(block_to_block_type(text_b))
+        self.assertEqual(block_types, check_types)
